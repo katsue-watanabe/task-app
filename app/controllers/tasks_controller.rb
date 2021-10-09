dcdd05bf3dce1105ea1668ad3a52d24c2fb20858
katsue-watanabe <k-losmama@docomo.ne.jp>
2021-10-09 10:31:30 +0000

    task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -11,3 +11,30 @@ end
 def new
   @task = Task.new
 end
+
+def created
+  if @task = @user.task.build(task_params)
+     flash[:success] = "タスクを新規作成しました。"
+     redirect_to tasus_index_url @user
+  else
+     render :new
+  end
+end
+
+def edit
+end
+
+def update
+end
+
+def destroy
+  @task.destroy
+  flash[:success] = "タスクを削除しました。"
+  redirect_to tasks_index_url @user
+end
+
+private
+
+  def task_params
+    params.require(:task).parmit(:name, :email, :password, :password_confirmation)
+  end
